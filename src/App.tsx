69d3f7716b022344d9595b7123f65bb74f714c43
Pablo Obando <josepabloobandogonzalez@gmail.com>
2020-12-16 12:34:03 -0600

    center button enrollment

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,43 @@
-import * as React from "react";
-import styled, { ThemeProvider } from "styled-components";
-import { GlobalStyles } from "./components/GlobalStyles";
-import Button from "./components/Button";
-import ToggleTheme from "./components/ToggleTheme";
-import { lightTheme, darkTheme } from "./lib/theme";
-import { useDarkMode } from "./hooks/useDarkmode";
+import * as React from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from './components/GlobalStyles'
+import Button from './components/Button'
+import ToggleTheme from './components/ToggleTheme'
+import { lightTheme, darkTheme } from './lib/theme'
+import { useDarkMode } from './hooks/useDarkmode'
 import useBoolean from './hooks/useBoolean'
 import Modal from './components/Modal'
 import FormSteps from './components/FormSteps'
 
-const ContainerCenter = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
 const ButtonEnrollment = styled(Button)`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
   background-color: #3dd28d;
   color: #fff;
   margin-top: 8rem;
 `
 
 const App = () => {
-  const [theme, themeToggler, mountedComponent] = useDarkMode();
-  const themeMode = theme === "light" ? lightTheme : darkTheme;
+  const [theme, themeToggler, mountedComponent] = useDarkMode()
+  const themeMode = theme === 'light' ? lightTheme : darkTheme
   const [isEnrollment, toggleIsEnrollment] = useBoolean()
 
-  if (!mountedComponent) return <div />;
+  if (!mountedComponent) return <div />
 
   return (
     <ThemeProvider theme={themeMode}>
       <GlobalStyles />
       <ToggleTheme theme={theme} toggleTheme={themeToggler} />
-      <ContainerCenter>
-        <ButtonEnrollment onClick={toggleIsEnrollment}>
-          <strong>enrollment</strong>
-        </ButtonEnrollment>
-      </ContainerCenter>
+      <ButtonEnrollment onClick={toggleIsEnrollment}>
+        <strong>enrollment</strong>
+      </ButtonEnrollment>
       <Modal isOpen={isEnrollment}>
         <FormSteps onClose={toggleIsEnrollment} />
       </Modal>
     </ThemeProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
