8e4569bb31a7926fd0dd653d0b9240fc4f13c2ce
Pablo Obando <josepabloobandogonzalez@gmail.com>
2021-01-26 17:59:36 -0600

    add some validations at input question

--- src/components/InputQuestion.tsx
+++ src/components/InputQuestion.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, ChangeEvent } from 'react'
 import styled from 'styled-components'
 import Toggle from './Toggle'
 
@@ -11,34 +11,43 @@ const ContainerQuestion = styled.div`
 const InputQuestion: React.FC<{
   id: string
   value: string
-  onChange: () => void
-}> = ({ children, id, value, onChange }) => {
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+  onClear: (key: string) => void
+}> = ({ children, id, value, onChange, onClear }) => {
   const [isChecked, setIsChecked] = useState(false)
-  const toggleIsChecked = () => setIsChecked((prevValue) => !prevValue)
+  const toggleIsChecked = () => {
+    onClear(id)
+    setIsChecked((prevValue) => !prevValue)
+  }
+
+  const renderInput = () => {
+    if (!isChecked && value.length === 0) return null
+    return (
+      <label htmlFor={id} className='secondary-question'>
+        How much and how often?
+        <input
+          type='text'
+          id={id}
+          name={id}
+          required
+          value={value}
+          onChange={onChange}
+        />
+      </label>
+    )
+  }
 
   return (
     <>
       <ContainerQuestion>
         <h4>{children}</h4>
         <Toggle
-          isChecked={isChecked}
+          isChecked={isChecked || value.length > 0}
           toggleIsChecked={toggleIsChecked}
           id={id}
         />
       </ContainerQuestion>
-      {isChecked && (
-        <label htmlFor={id} className='secondary-question'>
-          How much and how often?
-          <input
-            type='text'
-            id={id}
-            name={id}
-            required
-            value={value}
-            onChange={onChange}
-          />
-        </label>
-      )}
+      {renderInput()}
     </>
   )
 }
