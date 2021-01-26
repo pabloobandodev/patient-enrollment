da21b72ffe38bc3fbe8128c2682e6cbe700024e0
Pablo Obando <josepabloobandogonzalez@gmail.com>
2021-01-26 10:21:20 -0600

    add some header styles

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,24 @@ const Header = styled.div`
   background: ${({ theme }) => theme.white};
   border-bottom: 2px solid ${({ theme }) => theme.gray};
   box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 3px 0px;
-  height: 70px;
+  padding: 1rem 0 1rem 0;
+  h3 {
+    font-size: 1.8rem;
+    letter-spacing: 0.1rem;
+  }
 `
 
-const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Typography />
-      <Header>
-        <h3 className='center'>Parsley Health</h3>
-      </Header>
-      <EnrollmentProvider>
-        <FormSteps />
-      </EnrollmentProvider>
-    </ThemeProvider>
-  )
-}
+const App: React.FC = () => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Typography />
+    <Header>
+      <h3 className='center'>Parsley Health</h3>
+    </Header>
+    <EnrollmentProvider>
+      <FormSteps />
+    </EnrollmentProvider>
+  </ThemeProvider>
+)
 
 export default App
