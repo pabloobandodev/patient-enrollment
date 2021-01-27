c9889de0fa8e580b6ce78db99564b0faffd43692
Pablo Obando <josepabloobandogonzalez@gmail.com>
2021-01-26 18:01:18 -0600

    add and remove some constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,10 @@
-import { FormData } from '../context/types'
+export const SITE_TITLE = 'Parsley Health'
 
-export const SITE_TITLE = 'Patient Enrollment'
 export const terms =
   'Nullam quis risus eget urna mollis ornare vel eu leo. Aenean lacinia bibendum nulla sed consectetur. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit sit amet non magna. Etiam porta sem malesuada magna mollis euismod. Nullam quis risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vestibulum id ligula porta felis euismod semper. Donec ullamcorper nulla non metus auctor fringilla.'
+
 export const genders = ['Female', 'Male', 'Other']
+
 export const maritalStatus = [
   'Married',
   'Single',
@@ -13,132 +14,93 @@ export const maritalStatus = [
   'Widowed',
   'Other',
 ]
+
 export const typeConditions = [
   'cardiovascular',
   'gastrointestinal',
   'psychological',
   'other',
 ]
-export const formData: FormData = {
-  demographics: {
-    firstName: '',
-    lastName: '',
-    gender: '',
-    birth: '',
-    email: '',
-    phoneNumber: '',
-    streetAddress: '',
-    city: '',
-    state: '',
-    zip: '',
-    maritalStatus: '',
-  },
-  conditions: [
-    {
-      type: 'cardiovascular',
-      condition: 'High blood pressure',
-      isPositive: false,
-    },
-    {
-      type: 'cardiovascular',
-      condition: 'Cardiac Arrest',
-      isPositive: false,
-    },
-    {
-      type: 'cardiovascular',
-      condition: 'Arrhythmia',
-      isPositive: false,
-    },
-    {
-      type: 'cardiovascular',
-      condition: 'Coronary heart disease',
-      isPositive: false,
-    },
-    {
-      type: 'gastrointestinal',
-      condition: 'IBS',
-      isPositive: false,
-    },
-    {
-      type: 'gastrointestinal',
-      condition: "Crohn's Disease",
-      isPositive: false,
-    },
-    {
-      type: 'gastrointestinal',
-      condition: 'Gallstones',
-      isPositive: false,
-    },
 
-    {
-      type: 'psychological',
-      condition: 'Depression',
-      isPositive: false,
-    },
-    {
-      type: 'psychological',
-      condition: 'Anxiety',
-      isPositive: false,
-    },
-    {
-      type: 'psychological',
-      condition: 'Stress',
-      isPositive: false,
-    },
-    {
-      type: 'psychological',
-      condition: 'Insomnia',
-      isPositive: false,
-    },
-    {
-      type: 'other',
-      condition: 'Cancer',
-      isPositive: false,
-    },
-    {
-      type: 'other',
-      condition: 'Diabetes',
-      isPositive: false,
-    },
-  ],
-  questionnaire: [
-    {
-      primary: 'Do you smoke any tobacco products?',
-      answer: '',
-      secondary: 'How much and how often?',
-      value: '',
-    },
-    {
-      primary: 'Do you drink alcohol?',
-      answer: '',
-      secondary: 'How much and how often?',
-      value: '',
-    },
-    {
-      primary: 'Have you regularly used illicit drugs?',
-      answer: '',
-      secondary: 'How much and how often?',
-      value: '',
-    },
-    {
-      primary: 'Current medications',
-      answer: '',
-      secondary:
-        'Please list any medications you are currently taking including non-prescription medications, vitamins and supplements.',
-      value: [],
-    },
-    {
-      primary: 'Medication allergies or reactions',
-      answer: '',
-      secondary: 'Please list any medication allergies or reactions',
-      value: [],
-    },
-    {
-      primary:
-        'List any surgeries or hospital stays you have had and their approximate date / year',
-      answer: '',
-      secondary: 'Type of surgery or reason for hospitalization',
-      value: [],
-    },
-  ],
+export const conditions = [
+  {
+    type: 'cardiovascular',
+    condition: 'High blood pressure',
+    id: `cond-1`,
+  },
+  {
+    type: 'cardiovascular',
+    condition: 'Cardiac arrest',
+    id: `cond-2`,
+  },
+  {
+    type: 'cardiovascular',
+    condition: 'Arrhythmia',
+    id: `cond-3`,
+  },
+  {
+    type: 'cardiovascular',
+    condition: 'Coronary heart disease',
+    id: `cond-4`,
+  },
+  {
+    type: 'gastrointestinal',
+    condition: 'IBS',
+    id: `cond-5`,
+  },
+  {
+    type: 'gastrointestinal',
+    condition: "Crohn's disease",
+    id: `cond-6`,
+  },
+  {
+    type: 'gastrointestinal',
+    condition: 'Gallstones',
+    id: `cond-7`,
+  },
+
+  {
+    type: 'psychological',
+    condition: 'Depression',
+    id: `cond-8`,
+  },
+  {
+    type: 'psychological',
+    condition: 'Anxiety',
+    id: `cond-9`,
+  },
+  {
+    type: 'psychological',
+    condition: 'Stress',
+    id: `cond-10`,
+  },
+  {
+    type: 'psychological',
+    condition: 'Insomnia',
+    id: `cond-11`,
+  },
+  {
+    type: 'other',
+    condition: 'Cancer',
+    id: `cond-12`,
+  },
+  {
+    type: 'other',
+    condition: 'Diabetes',
+    id: `cond-13`,
+  },
+]
+
+type IQuestionnaire = {
+  [key: string]: string
+}
+
+export const questionnaireLabels: IQuestionnaire = {
+  alcohol: 'Do you drink alcohol?',
+  allergies: 'Medication allergies or reactions',
+  tobacco: 'Do you smoke any tobacco products?',
+  drugs: 'Have you regularly used illicit drugs?',
+  medications: 'Current medications',
+  surgeries:
+    'List any surgeries or hospital stays you have had and their approximate date / year',
 }
