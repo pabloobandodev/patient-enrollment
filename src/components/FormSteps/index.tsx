344e6940c48e9f507347eb9e70dc1477a04180f9
Pablo Obando <josepabloobandogonzalez@gmail.com>
2021-01-26 18:11:35 -0600

    change demographic component to generalInfo at index

--- src/components/FormSteps/index.tsx
+++ src/components/FormSteps/index.tsx
@@ -1,20 +1,28 @@
 import React from 'react'
-import Demographic from './demographic'
+import Form from '../Form'
+import GeneralInfo from './general-info'
 import Conditions from './conditions'
 import Questionnaire from './questionnaire'
 import Summary from './summary'
 import Submit from './submit'
-import Form from '../Form'
 import usePagination from '../../hooks/usePagination'
+import useEnrollment from '../../hooks/useEnrollment'
 
 const FormPagination: React.FC = () => {
-  const { index, onNext, onPrev } = usePagination()
+  const { index, onNext, onPrev, setIndex } = usePagination()
+  const { submitEnrollment } = useEnrollment()
+
   return (
-    <Form>
-      <Demographic isVisible={index === 0} onNext={onNext} />
+    <Form onSubmit={submitEnrollment}>
+      <GeneralInfo isVisible={index === 0} onNext={onNext} />
       <Conditions isVisible={index === 1} onPrev={onPrev} onNext={onNext} />
       <Questionnaire isVisible={index === 2} onPrev={onPrev} onNext={onNext} />
-      <Summary isVisible={index === 3} onPrev={onPrev} onNext={onNext} />
+      <Summary
+        isVisible={index === 3}
+        onPrev={onPrev}
+        onNext={onNext}
+        setIndex={setIndex}
+      />
       <Submit isVisible={index === 4} onPrev={onPrev} />
     </Form>
   )
