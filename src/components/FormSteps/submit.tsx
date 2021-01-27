8ec0ae99eefef39d40c2192cd085596d6dd012e1
Pablo Obando <josepabloobandogonzalez@gmail.com>
2021-01-26 18:11:05 -0600

    finish submit component

--- src/components/FormSteps/submit.tsx
+++ src/components/FormSteps/submit.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { terms } from '../../lib/constants'
+import isEmpty from '../../lib/isEmpty'
 import Button from '../Button'
+import FormTitle from '../FormTitle'
 
-const ContainerText = styled.div`
+const Container = styled.fieldset`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
   flex-grow: 1;
+  overflow-y: auto;
 `
 const ContainerButton = styled.div`
   flex-grow: 0.5;
@@ -17,22 +19,82 @@ const ContainerButton = styled.div`
   width: 100%;
   padding-bottom: 2rem;
 `
+const ButtonsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`
 const StyledButton = styled(Button)`
+  width: 48%;
   background-color: ${(props) => props.color};
   border-color: ${(props) => props.color};
 `
+const Li = styled.li`
+  margin-bottom: 0.5rem;
+`
+const Small = styled.small`
+  font-size: 0.8rem;
+  margin-bottom: 1rem;
+  margin-left: 0.3rem;
+`
+const Span = styled.span`
+  border-bottom: 1px solid ${({ theme }) => theme.grannysmith};
+  color: ${({ theme }) => theme.grannysmith};
+  padding-bottom: 0.3rem;
+`
 
-const Submit: React.FC<any> = ({ isVisible, onNext, onPrev }) => {
+type IProps = {
+  isVisible: boolean
+  onPrev: () => void
+}
+
+const Submit: React.FC<IProps> = ({ isVisible, onPrev }) => {
+  const [isAgree, setIsAgree] = useState(false)
+  const toggleIsAgree = () => setIsAgree((prevValue) => !prevValue)
+
   if (!isVisible) {
     return null
   }
+
   return (
     <>
-      <ContainerText>Submit</ContainerText>
+      <Container>
+        <FormTitle number='5' subTitle='Read the terms'>
+          Terms
+        </FormTitle>
+        <ul>
+          {terms
+            .split('.')
+            .filter((term) => !isEmpty(term))
+            .map((term, i) => (
+              <Li key={`term-${i}`}>{term}</Li>
+            ))}
+        </ul>
+        <label htmlFor='terms'>
+          <input
+            type='checkbox'
+            id='terms'
+            name='terms'
+            required
+            onChange={toggleIsAgree}
+            checked={isAgree}
+          />
+          <Small>
+            I have read and agree to the <Span>Terms of Use</Span>,{' '}
+            <Span>Privacy Policy</Span> and <Span>Membership Agreement</Span>.
+          </Small>
+        </label>
+      </Container>
       <ContainerButton>
-        <StyledButton color={'#29e0ad'} onClick={onNext}>
-          submit
-        </StyledButton>
+        <ButtonsContainer>
+          <StyledButton type='button' color={'#474747'} onClick={onPrev}>
+            Back
+          </StyledButton>
+          <StyledButton type='submit' disabled={!isAgree}>
+            Submit
+          </StyledButton>
+        </ButtonsContainer>
       </ContainerButton>
     </>
   )
