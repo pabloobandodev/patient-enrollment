b70fc0c5ed50aeb1ee896afcc0757904915e18d9
Pablo Obando <josepabloobandogonzalez@gmail.com>
2021-01-26 18:04:59 -0600

    add some types at enrollment context

--- src/context/enrollment.tsx
+++ src/context/enrollment.tsx
@@ -1,15 +1,34 @@
-import React, { useState, Dispatch, SetStateAction } from 'react'
+import React, { useState } from 'react'
+
+export type IObjectData = {
+  [key: string]: string
+}
+
+type IData = {
+  generalInfo: IObjectData
+  conditionIds: string[]
+  questionnaire: IObjectData
+}
 
 const EnrollmentContext = React.createContext<{
-  data: {}
-  setData: Dispatch<SetStateAction<{}>>
+  data: IData
+  setData: any
 }>({
-  data: {},
-  setData: () => {},
+  data: {
+    generalInfo: {},
+    conditionIds: [],
+    questionnaire: {},
+  },
+  setData: (data: {}) => {},
 })
 
 export const EnrollmentProvider: React.FC = ({ children }) => {
-  const [data, setData] = useState({})
+  const [data, setData] = useState({
+    generalInfo: {},
+    conditionIds: [],
+    questionnaire: {},
+  })
+
   return (
     <EnrollmentContext.Provider value={{ data, setData }}>
       {children}
--- src/context/types.ts
+++ src/context/types.ts
@@ -1,36 +1,15 @@
-export type Condition = {
+export type IObjectData = {
+  [key: string]: string
+}
+
+export type IData = {
+  generalInfo: IObjectData
+  conditionIds: string[]
+  questionnaire: IObjectData
+}
+
+export type ICondition = {
   type: string
   condition: string
-  isPositive: boolean
-}
-
-export type Question = {
-  primary: string
-  answer: string
-  secondary: string
-  value: [] | string
-}
-
-export type FormData = {
-  demographics: {
-    firstName: string
-    lastName: string
-    gender: string
-    birth: string
-    email: string
-    phoneNumber: string
-    streetAddress: string
-    city: string
-    state: string
-    zip: string
-    maritalStatus: string
-  }
-  conditions: Condition[]
-  questionnaire: Question[]
-}
-
-export type ActionController = {
-  type: string
-  payload?: any
-  fieldName?: any
+  id: string
 }
